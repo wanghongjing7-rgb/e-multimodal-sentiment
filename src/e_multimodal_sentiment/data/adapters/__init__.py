51e91f3063bc2631219376f69ea08da0e96fd0b9
whx <302341@qq.com>
2026-09-23 18:24:53 +0800

    feat(data): bridge aligned MOSEI data to MMSA MulT

--- src/e_multimodal_sentiment/data/adapters/__init__.py
+++ src/e_multimodal_sentiment/data/adapters/__init__.py
@@ -1,6 +1,7 @@
 """Serialization adapters that convert external data into SampleSchema."""
 
 from .base import BaseAdapter
+from .attachment2_aligned import Attachment2AlignedAdapter
 from .mosei_pickle import MoseiPickleAdapter
 
-__all__ = ["BaseAdapter", "MoseiPickleAdapter"]
+__all__ = ["Attachment2AlignedAdapter", "BaseAdapter", "MoseiPickleAdapter"]
---
+++ src/e_multimodal_sentiment/integrations/__init__.py
@@ -0,0 +1,5 @@
+"""Thin boundaries to fixed third-party interfaces."""
+
+from .mmsa import MMSAMulTInputs, batch_to_mmsa_mult_inputs
+
+__all__ = ["MMSAMulTInputs", "batch_to_mmsa_mult_inputs"]
